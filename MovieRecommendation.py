483d1b3a64e55b7a6d6a494b5ae19c670c5ac814
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-11-16 14:18:08 -0800

    dataset selection

--- MovieRecommendation.py
+++ MovieRecommendation.py
@@ -9,11 +9,23 @@ import pandas as pd
 
 
 r_cols = ['user_id','movie_id','rating']
-ratings = pd.read_csv('C:\Users\aakash.chotrani\Desktop\DataScience-Python3\ml-100k\u.data', sep = '\t', names = r_cols, usecols = range(3))
+ratings = pd.read_csv('C:/Users/aakash.chotrani/Desktop/DataScience-Python3/ml-100k/u.data', sep = '\t', names = r_cols, usecols = range(3))
 
 
 m_cols = ['movie_id','title']
-movies = pd.read_csv('C:\Users\aakash.chotrani\Desktop\DataScience-Python3\ml-100k\u.item', sep = '|', names = m_cols, usecols = range(2))
+movies = pd.read_csv('C:/Users/aakash.chotrani/Desktop/DataScience-Python3/ml-100k/u.item', sep = '|', names = m_cols, usecols = range(2), encoding = 'latin-1')
+
+ratings = pd.merge(movies,ratings)
+
+movieRatings = ratings.pivot_table(index = ['user_id'], columns = ['title'], values = 'rating')
+
+starWarsRatings = movieRatings['Star Wars (1977)']
 
 
-raings = pd.merge(movies,ratings)
+similarMovies = movieRatings.corrwith(starWarsRatings)
+similarMovies = similarMovies.dropna()
+df = pd.DataFrame(similarMovies)
+df.head(10)
+
+
+similarMovies.order(ascending = False)
